5af723e2758799f7ffa185e8c89f0527a7871103
Ceren Koç <111969447+ceereenkoc@users.noreply.github.com>
2022-10-22 21:42:06 +0300

    Ters Üçgen Yapma
    Basamak sayısı kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*) ile ekrana ters üçgen çizen program

---
+++ tersucgen/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen basamak sayısı giriniz: ");
+        int n = input.nextInt();
+
+        for(int i=0; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int k =1; k <=(2*n-(2*i-1)); k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
